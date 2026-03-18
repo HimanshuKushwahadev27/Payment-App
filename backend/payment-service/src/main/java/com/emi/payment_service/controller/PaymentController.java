dcefba192d3d64d0334380fa7778d3ef77c9a6a4
himanshuamaurya12 <himanshumaurya447756@gmail.com>
2026-03-18 14:03:46 +0530

    PA-4 added controller in payment

--- backend/payment-service/src/main/java/com/emi/payment_service/controller/PaymentController.java
+++ backend/payment-service/src/main/java/com/emi/payment_service/controller/PaymentController.java
@@ -1,13 +1,50 @@
 package com.emi.payment_service.controller;
 
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emi.payment_service.RequestDtos.RequestPaymentDto;
+import com.emi.payment_service.ResponseDtos.ResponsePaymentDto;
+import com.emi.payment_service.service.PaymentService;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/payment")
 @RequiredArgsConstructor
 public class PaymentController {
-
+	
+	 private final PaymentService paymentService;
+	
+	@PostMapping("/charge")
+	public ResponseEntity<ResponsePaymentDto> charge(
+			@RequestBody @Valid RequestPaymentDto request,
+			@AuthenticationPrincipal Jwt jwt,
+			@RequestHeader("Idempotency-Key") String IdempotencyKey){
+		
+		return ResponseEntity.ok(
+				paymentService.charge(
+						request,
+						UUID.fromString(IdempotencyKey), 
+						UUID.fromString(jwt.getSubject())
+						)
+				);
+	}
+	
+	@PostMapping("/webhook")
+	public ResponseEntity<Void> webhook(
+	        @RequestBody String payload,
+	        @RequestHeader("Stripe-Signature") String sigHeader) {
+		paymentService.handleWebhook(payload, sigHeader);
+	    return ResponseEntity.ok().build();
+	}
 }
