ad11e7d61fed6fa2f1c9e2e27a7232488896e5b3
himanshuamaurya12 <himanshumaurya447756@gmail.com>
2026-03-20 19:32:18 +0530

    PA-5 added openAPI config and security

--- backend/wallet-service/src/main/java/com/emi/wallet_service/config/GeneralConfig.java
+++ backend/wallet-service/src/main/java/com/emi/wallet_service/config/GeneralConfig.java
@@ -6,6 +6,10 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 
@@ -26,5 +30,25 @@ public class GeneralConfig {
      ObjectMapper objectMapper() {
         return new ObjectMapper();
      }
+     
+     
+     
+     
+ 	
+ 	@Bean
+ 	OpenAPI openApiConfig() {
+ 		return new OpenAPI()
+ 				.info(new Info()
+ 						.title("Wallet Service API")
+ 						.description("API documentation for Wallet-service")
+ 						.version("2.0")
+ 						.license(new License()
+ 								.name("Apache 2.0")
+ 								.url("http://springdoc.org")
+ 								))
+ 				.externalDocs(new ExternalDocumentation()
+ 						.description("Link to external documentation")
+ 						.url("https://wallet-demo.com/docs"));
+ 	}
 
 }
--- backend/wallet-service/src/main/java/com/emi/wallet_service/config/SecConfiguration.java
+++ backend/wallet-service/src/main/java/com/emi/wallet_service/config/SecConfiguration.java
@@ -1,8 +1,25 @@
 package com.emi.wallet_service.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecConfiguration {
 
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/public/**").permitAll()
+                    .anyRequest().authenticated()
+			)
+			.oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+			.build()
+			;
+	}
 }
