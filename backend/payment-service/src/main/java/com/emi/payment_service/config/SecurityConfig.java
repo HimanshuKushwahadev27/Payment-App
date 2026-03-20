a8cb7e56277ffe32bd331c68e103a78562bbc87b
himanshuamaurya12 <himanshumaurya447756@gmail.com>
2026-03-20 19:33:24 +0530

    PA-4 added openApi and security config

--- backend/payment-service/src/main/java/com/emi/payment_service/config/SecurityConfig.java
+++ backend/payment-service/src/main/java/com/emi/payment_service/config/SecurityConfig.java
@@ -3,11 +3,16 @@ package com.emi.payment_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 
@@ -25,10 +30,13 @@ public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
-			.csrf(csrf -> csrf.disable())
-			.authorizeHttpRequests(authorize -> authorize
-				.anyRequest().permitAll()
-			).build()
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/public/**").permitAll()
+                    .anyRequest().authenticated()
+			)
+			.oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+			.build()
 			;
 	}
 	
@@ -37,4 +45,21 @@ public class SecurityConfig {
      ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
+    
+    
+ 	@Bean
+ 	OpenAPI openApiConfig() {
+ 		return new OpenAPI()
+ 				.info(new Info()
+ 						.title("Payment Service API")
+ 						.description("API documentation for Payment-service")
+ 						.version("2.0")
+ 						.license(new License()
+ 								.name("Apache 2.0")
+ 								.url("http://springdoc.org")
+ 								))
+ 				.externalDocs(new ExternalDocumentation()
+ 						.description("Link to external documentation")
+ 						.url("https://payment-demo.com/docs"));
+ 	}
 }
