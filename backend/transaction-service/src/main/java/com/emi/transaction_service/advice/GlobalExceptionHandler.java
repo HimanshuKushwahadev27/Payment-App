9fbccfc86986a84683c39844d92435e76381b9d7
Eminence <himanshumaurya447756@gmail.com>
2026-04-15 16:15:44 +0000

    added global exception configurations

--- backend/transaction-service/src/main/java/com/emi/transaction_service/advice/GlobalExceptionHandler.java
+++ backend/transaction-service/src/main/java/com/emi/transaction_service/advice/GlobalExceptionHandler.java
@@ -1,8 +1,28 @@
 package com.emi.transaction_service.advice;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.emi.transaction_service.exception.TransactionNotFoundException;
+import com.emi.transaction_service.exception.UnauthorizedException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+
+	@ExceptionHandler(TransactionNotFoundException.class)
+	public ResponseEntity<?> handleBookExistsFound(TransactionNotFoundException ex){
+		return ResponseEntity
+				.status(404)
+				.body(ex.getMessage());
+	}
+
+	@ExceptionHandler(UnauthorizedException.class)
+	public ResponseEntity<?> handleBookExistsFound(UnauthorizedException ex){
+		return ResponseEntity
+				.status(401)
+				.body(ex.getMessage());
+	}
 
 }
--- backend/wallet-service/src/main/java/com/emi/wallet_service/advice/GlobalExceptionHandler.java
+++ backend/wallet-service/src/main/java/com/emi/wallet_service/advice/GlobalExceptionHandler.java
@@ -1,8 +1,48 @@
 package com.emi.wallet_service.advice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.emi.wallet_service.exception.AccountNotExistsException;
+import com.emi.wallet_service.exception.LedgerNotFoundException;
+import com.emi.wallet_service.exception.UnauthorizedException;
+import com.emi.wallet_service.exception.WalletNotExistsException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+  @ExceptionHandler(AccountNotExistsException.class)
+	public ResponseEntity<?> handleContentNotFound(AccountNotExistsException  ex){
+		return  ResponseEntity
+				.status(HttpStatus.NOT_FOUND)
+				.body(ex.getMessage());
+	}
+
+
+   @ExceptionHandler(LedgerNotFoundException.class)
+	public ResponseEntity<?> handleContentNotFound(LedgerNotFoundException  ex){
+		return  ResponseEntity
+				.status(HttpStatus.NOT_FOUND)
+				.body(ex.getMessage());
+	}
+
+  @ExceptionHandler(UnauthorizedException.class)
+  public ResponseEntity<?> handleContentNotFound(UnauthorizedException  ex){
+    return  ResponseEntity
+        .status(HttpStatus.UNAUTHORIZED)
+        .body(ex.getMessage());
+  }
+
+  @ExceptionHandler(WalletNotExistsException.class)
+  public ResponseEntity<?> handleContentNotFound(WalletNotExistsException  ex){
+    return  ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(ex.getMessage());
+  }
+
+
+
+  
 }
